d3deff56900108d4f3e990bacf0ad89fb83fc119
Tito <titosi099@gmail.com>
2016-10-28 10:54:10 -0300

    Finalização dos teste de integração (apenas GET) com sequelize

--- test/integration/routes/students.js
+++ test/integration/routes/students.js
@@ -1,10 +1,35 @@
 describe('Routes Students', () => {
-  describe('Route /students', () => {
-    it('Should return a list of students', () => {
+  const Students = app.db.models.Students;
+  const defaultStudent = {
+    id: 1,
+    name: 'Suricate Seboso',
+  };
+
+  beforeEach((done) => {
+    Students.destroy({ where: {} })
+    .then(() => Students.create(defaultStudent))
+    .then(() => done());
+  });
+
+  describe('Route GET /students', () => {
+    it('Should return a list of students', (done) => {
       request
-      .get('./students')
+      .get('/students')
       .end((err, res) => {
-        expect(res.body[0].id).to.be.eql(1);
+        expect(res.body[0].id).to.be.eql(defaultStudent.id);
+        expect(res.body[0].name).to.be.eql(defaultStudent.name);
+        done();
+      });
+    });
+  });
+  describe('Route GET /students/{id}', () => {
+    it('Should return a student', (done) => {
+      request
+      .get('/students/1')
+      .end((err, res) => {
+        expect(res.body.id).to.be.eql(defaultStudent.id);
+        expect(res.body.name).to.be.eql(defaultStudent.name);
+        done();
       });
     });
   });
--- test/integration/routes/users.js
+++ test/integration/routes/users.js
@@ -1,10 +1,39 @@
 describe('Routes Users', () => {
+  const Users = app.db.models.Users;
+  const defaultUser = {
+    id: 1,
+    name: 'Roberio',
+    email: 'roberio@mail.com',
+    password: 'rea123',
+  };
+
+  beforeEach((done) => {
+    Users.destroy({ where: {} })
+    .then(() => Users.create(defaultUser))
+    .then(() => done());
+  });
+
   describe('Route GET /users', () => {
-    it('Should return a list of users', () => {
+    it('Should return a list of users', (done) => {
       request
       .get('/users')
       .end((err, res) => {
-        expect(res.body[0].id).to.be.eql(1);
+        expect(res.body[0].id).to.be.eql(defaultUser.id);
+        expect(res.body[0].name).to.be.eql(defaultUser.name);
+        expect(res.body[0].email).to.be.eql(defaultUser.email);
+        done();
+      });
+    });
+  });
+  describe('Route GET /users/{id}', () => {
+    it('Should return a user', (done) => {
+      request
+      .get('/users/1')
+      .end((err, res) => {
+        expect(res.body.id).to.be.eql(defaultUser.id);
+        expect(res.body.name).to.be.eql(defaultUser.name);
+        expect(res.body.email).to.be.eql(defaultUser.email);
+        done();
       });
     });
   });
